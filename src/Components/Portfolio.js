01ab7e01361284d7e2a7c35fff3bd065ad95375b
ashwantmanikoth <ashwanta75@gmail.com>
2024-09-05 18:55:20 -0300

    removing unused variables

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const Portfolio = ({ data }) => {
   if (data) {
-
-    
-    var projects = data.projects.map(function (projects) {
       var projectImage = "images/portfolio/" + projects.image;
       return (
         <div key={projects.title} className="columns portfolio-item">
